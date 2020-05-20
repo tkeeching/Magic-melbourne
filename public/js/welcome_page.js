9f3648a6da344de198f24991c6df86bdcdc6a1ef
Kevi G <kevgarciamusic@hotmail.com>
2020-05-20 14:56:07 +1000

    started integrating front page to index

--- public/js/welcome_page.js
+++ public/js/welcome_page.js
@@ -1,10 +1,10 @@
-const searchBtn = document.querySelector('.welcome_page__user_dropdown_search_btn');
+// const searchBtn = document.querySelector('.welcome_page__user_dropdown_search_btn');
 
-const handleSearch = (e) => {
-  e.preventDefault();
-  const userQueryCategory = e.target.form.children[1].value;
-  const userQueryBudget = e.target.form.children[3].value;
-  // to be added - query to database
-}
+// const handleSearch = (e) => {
+//   // e.preventDefault();
+//   const userQueryCategory = e.target.form.children[1].value;
+//   const userQueryBudget = e.target.form.children[3].value;
+//   // to be added - query to database
+// }
 
-searchBtn.addEventListener('click', handleSearch);
+// searchBtn.addEventListener('click', handleSearch);
--- server.js
+++ server.js
@@ -29,33 +29,43 @@ app.get('/', (req, res) => {
 
 app.get('/index', (req, res) => {
     
-    //query the API for the relevant interest
-    // switch (req.params) {
-    //     case food:
-                    // then variables is best restaurants
-    //         break;
-    //     case POI:
-                    // then variables is POI
-    //         break;
-    
-    //     default:
-    //         break;
-    // }
+    // res.json(req.query.category)
 
-    let url = "https://api.sygictravelapi.com/1.2/en/places/list?location=-37.820,144.981&query=hotels&limit=20"
+    // //query the API for the relevant interest
+
+    // // switch (req.params) {
+    // //     case food:
+    //                 // then variables is best restaurants
+    // //         break;
+    // //     case POI:
+    //                 // then variables is POI
+    // //         break;
+    
+    // //     default:
+    // //         break;
+    // // }
+
+    let url = `https://api.sygictravelapi.com/1.2/en/places/list?parents=city:381&categories=${ req.query.category }&limit=20`
+
+    // res.send(url)
     
     axios.get(url,{
         headers: {
             'x-api-key': 'BNLiHyXDsUa1OhdwsHho47y6rO0HKcNa5BWnofl7'
           }
     }).then(response =>{
+        //clean up the date to provide us the relevant information
         let arrOfPlaces = response.data.data.places
         let arrOfInstances = []
         arrOfPlaces.forEach(place =>{
             let instacePlave = {id: place.id, name: place.name, location: place.location, description: place.perex, image: place.thumbnail_url}
             arrOfInstances.push(instacePlave)
         })
-        res.json(arrOfInstances)
+        // res.json(arrOfPlaces)
+        // res.json(arrOfInstances)
+        res.render('index', {
+            attractions: arrOfInstances
+        })
     })
 })
 
