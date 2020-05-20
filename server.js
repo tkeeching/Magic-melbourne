8990aef6347e37611e253304ffbd4db16bbee084
Kevi G <kevgarciamusic@hotmail.com>
2020-05-20 22:55:44 +1000

    tidy code, pop-up work, install session

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const geoJSON = require('./library/geoJSON_module');
+const session = require('express-session')
 
 
 ////////////   Middleware /////////////
@@ -20,43 +21,6 @@ app.get('/', (req, res) => {
     res.render('welcome_page')
 })
 
-
-app.get('/api/index', (req, res) => {
-    
-    // res.send('hello')
-    let url = `https://api.sygictravelapi.com/1.2/en/places/list?parents=city:381&categories=${ req.query.category }&limit=20`
-
-    // res.send(url)
-    
-    axios.get(url,{
-        headers: {
-            'x-api-key': 'BNLiHyXDsUa1OhdwsHho47y6rO0HKcNa5BWnofl7'
-          }
-    }).then(response =>{
-        
-        // res.json(response.data)
-
-        // clean up the date to provide us the relevant information
-        let arrOfPlaces = response.data.data.places
-        let arrOfInstances = []
-
-        arrOfPlaces.forEach(place =>{
-            let instancePlace = {id: place.id, name: place.name, location: place.location, description: place.perex, image: place.thumbnail_url}
-            arrOfInstances.push(instancePlace)
-        })
- 
-        let geoJData = []
-        arrOfInstances.forEach(instance => {
-            geoJData.push(geoJSON.convertToGeoJSON(response.data));
-        })
-
-        // res.render('index', {
-        //     attractions: arrOfInstances,
-        //     geoJData: geoJData[0]
-        // })
-        res.json(geoJData[0])
-    })
-})
 
 app.get('/index', (req, res) => {
     
@@ -84,14 +48,13 @@ app.get('/index', (req, res) => {
 
     // let geoJData = geoJSON.convertToGeoJSON(arrOfInstances);
 
-
-
     res.render('index', {
         attractions: arrOfInstances,
         // geoJData: geoJData, // not required since we made a call to /api/geojson to retrieve the geojson data instead
     })
   })
 })
+
 
 // geoJSON output
 app.get('/api/geojson', (req, res) => {
