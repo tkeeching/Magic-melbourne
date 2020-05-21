5da7d9c1808545ee54ae4c7a7674290df6515430
Kevi G <kevgarciamusic@hotmail.com>
2020-05-21 18:16:04 +1000

    merge a bunch of things

--- public/javascript/client-index.js
+++ public/javascript/client-index.js
@@ -1,27 +1,29 @@
-
 console.log('ITS LINKED NOW')
 
 let arrOfattractions = document.querySelectorAll('.fa')
 let arrOfSelectedAttractions = []
-
 arrOfattractions.forEach(att => {
-    att.addEventListener('click', e =>{
-        // let attId = e.target.id
-        // arrOfSelectedAttractions.push(attId)
+    att.addEventListener('click', e => {
+        let attId = e.target.id
+        arrOfSelectedAttractions.push(attId)
         e.target.style.color = 'red'
-
-        console.log(e.target.dataset.object);
-
-        const url = '/attractions'
+        const url = 'http://localhost:8080/attractions'
         const params = {
-            attraction: e.target.dataset.object
+            id: e.target.id,
+            idArr: arrOfSelectedAttractions
         }
+        axios.post(url, params)
 
-        axios.post(url,params)
-        // axios({
-        //     method: 'post',
-        //     url: url,
-        //     data: e.target.dataset.object
-        // })
+        // V2 => this passes the event target attraction to the server
+        
+        // console.log(e.target.dataset.object);
+
+        // const url = '/attractions'
+        // const params = {
+        //     attraction: e.target.dataset.object
+        // }
+
+        // axios.post(url,params)
+
     })
 })
