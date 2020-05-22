1f00d75d4a83394aff5ba6335d3a507397887dbc
Kee Tee <tkeeching@gmail.com>
2020-05-22 16:52:00 +1000

    Update session to work on HTTPS

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 
 
 ////////////  Session setup  /////////////
+app.set('trust proxy', 1)
 app.use(session({
   name: 'SessionCookie',
   genid: function (req) {
