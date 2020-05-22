4a57a5bf473185c2b2d441817403532f9e52bfdb
Kevi G <kevgarciamusic@hotmail.com>
2020-05-22 14:25:12 +1000

    port problem

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ////////////   Modules  /////////////
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 // if (!process.env.PRODUCTION) {
 //   const morgan = require('morgan');
 // } 
