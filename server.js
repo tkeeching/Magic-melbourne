7230544c6b9d00b1ae371cc108ff8b7f84a70d39
Kee Tee <tkeeching@gmail.com>
2020-09-05 15:50:39 +1000

    Add media rule

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-if (!process.env.PRODUCTION) {
-  const morgan = require('morgan');
-} 
+const morgan = !process.env.PRODUCTION ? require('morgan') : '';
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const geoJSON = require('./library/geoJSON_module');
